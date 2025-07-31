a07a400d98904290469bce29c46a6c63d2939907
rikzafauzannf <rikzarazor491@gmail.com>
2025-07-31 19:25:38 +0700

    test push

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,30 +1,37 @@
 // frontend/src/app/login/page.tsx
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useAuth } from '@/contexts/AuthContext';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const { login } = useAuth();
   const router = useRouter();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
     // Simple role-based login simulation
-    if (username === 'admin' || username === 'santri' || username === 'orangtua') {
+    if (
+      username === "admin" ||
+      username === "santri" ||
+      username === "orangtua"
+    ) {
       login(username);
-      router.push('/dashboard');
+      router.push("/dashboard");
     } else {
-      alert('Username tidak valid. Coba: admin, santri, atau orangtua');
+      alert("Username tidak valid. Coba: admin, santri, atau orangtua");
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="p-8 bg-white rounded-lg shadow-md w-96">
-        <h2 className="text-2xl font-bold text-center text-green-800 mb-6">Login</h2>
+        <h2 className="text-2xl font-bold text-center text-primary mb-6">
+          Login
+        </h2>
         <form onSubmit={handleLogin}>
           <div className="mb-4">
             <label className="block text-gray-700 mb-2" htmlFor="username">
@@ -39,12 +46,13 @@ export default function LoginPage() {
               placeholder="Gunakan 'admin', 'santri', atau 'orangtua'"
             />
           </div>
-          <button
+          <Button
             type="submit"
-            className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700"
+            className="w-full"
+            // className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700"
           >
             Login
-          </button>
+          </Button>
         </form>
       </div>
     </div>
